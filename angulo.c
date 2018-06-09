3a9fb3d8ee143d6cb8c266a0087a39f84cd6e13d
MSchottlender <martin.schottlender@gmail.com>
2018-06-08 21:12:49 -0300

    Update angulo.c

--- angulo.c
+++ angulo.c
@@ -1,50 +1,40 @@
 #include <avr/io.h>
 #include <math.h>
 
-#define XLOW 0X300; /*Aca pongo las posiciones de memoria donde busca la gravedad de cada eje*/
-#define XHIGH 0X301;  /*Que por estar en registros vienen separadas*/
-#define ZLOW 0X302;
-#define ZHIGH 0X303;
-#define ANGPOS 0X304;
+#define YLOW 0X300 /*Aca pongo las posiciones de memoria donde busca la gravedad de cada eje*/
+#define YHIGH 0X301  /*Que por estar en registros vienen separadas*/
+#define ZLOW 0X302
+#define ZHIGH 0X303
+#define ANGPOS 0X304
 
-#define pi 3.1415;
+#define pi 3.1415
 
-#define XMAX ???;
-#define XMIN ???;
-#define ZMAX ???;
-#define ZMIN ???; /*Esto se pone despues de calibrar el acelerometro (con arduino)*/
+#define YMAX 1006
+#define YMIN 357
+#define ZMAX 868
+#define ZMIN 237 /*Esto se pone despues de calibrar el acelerometro (con arduino)*/
 
-unsigned int volatile * const xal = (unsigned int *) XLOW;
-unsigned int volatile * const xah = (unsigned int *) XHIGH;
+unsigned int volatile * const yal = (unsigned int *) YLOW;
+unsigned int volatile * const yah = (unsigned int *) YHIGH;
 unsigned int volatile * const zal = (unsigned int *) ZLOW;
 unsigned int volatile * const zah = (unsigned int *) ZHIGH;
 unsigned int volatile * const angp = (unsigned int *) ANGPOS;
 
 void angulo(void)
 {
-	signed float x,z;
-	unsigned int xb,zb;
+	float y,z;
+	unsigned int yb,zb;
 	unsigned char ang;
 
-	xb=((*xah)<<8)+*xal;
+	yb=((*yah)<<8)+*yal;
 	zb=((*zah)<<8)+*zal;
+	
 
-	x=2*(xb-XMIN)/(XMAX-XMIN)-1;
-	z=2*(zb-ZMIN)/(ZMAX-ZMIN)-1;
-
-	ang=((char)atan2(z,x)+pi)*(180/(2*pi));
-
+	y=(200000*(yb-YMIN)/(YMAX-YMIN)-100000); /*mapeo multiplicado por 100000 para dar mas resolucion y presicion*/
+	z=(200000*(zb-ZMIN)/(ZMAX-ZMIN)-100000);
+	
+	ang=atan(z/y)*180/pi+90; /*Va de 0 a 180 grados*/
+	
 	*angp=ang;
 	
 }
-
-
-
-
-
-
-
-
-
-
-
