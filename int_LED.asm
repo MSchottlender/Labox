8b84aca19311fdecf9c603eab11f93d2db416f21
camistolo <40144517+camistolo@users.noreply.github.com>
2018-06-13 18:38:01 -0300

    Add files via upload

---
+++ int_LED.asm
@@ -0,0 +1,54 @@
+;
+; Interruption_switch.asm
+;
+; Created: 13/6/2018 17:52:31
+; Author : camis
+;
+.org INT_VECTORS_SIZE
+.org 0x000
+rjmp main
+
+.org 0x002
+rjmp interrupt
+
+.dseg
+.def AUX = r16
+.def AUX1 = r17
+.cseg
+main:
+sei		;seteo el flag I del SREG para habilitar las interrupciones
+ldi AUX, 0x01
+out EIMSK, AUX	;habilito la interrupcion INT0
+ldi AUX, 0x02
+sts EICRA, AUX	;configuro que la interrupcion se habilite cuando el nivel de la señal es bajo
+ldi AUX, 0x00
+out EIFR, AUX	;limpio las banderas que indican que una interrupcion se lleva a cabo
+cbi DDRC, 6		;coloco a PC6 como salida
+rjmp main
+
+interrupt:
+
+in AUX1, SREG
+push AUX1
+push AUX
+
+sbi PORTC, 6	;prendo el LED
+
+    ldi  r18, 41	;Delay de medio segundo
+    ldi  r19, 150
+    ldi  r20, 128
+L1: dec  r20
+    brne L1
+    dec  r19
+    brne L1
+    dec  r18
+    brne L1
+
+cbi PORTC, 6	;apago el LED
+
+
+pop AUX
+pop AUX1
+out SREG, AUX1
+
+reti
